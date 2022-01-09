cd3aad59f89e23adbd7e04dfebd4c689d71f8a99
Dalada89 <robin@braeutigam-nrw.de>
2022-01-09 16:11:44 +0100

    Improved unit test

--- test_read_website.py
+++ test_read_website.py
@@ -11,23 +11,62 @@ class TestStringMethods(unittest.TestCase):
         with open(r'.\test_data\test_side_error.html', 'r', encoding='utf-8') as html:
             cls.webpage = html.read()
         with open(r'.\test_data\db.json', 'r') as jsonfile:
-            cls.result = json.load(jsonfile)
-
-    def test_read(self):
-        further_data = {
+            cls.truths = json.load(jsonfile)
+        cls.further_data = {
             'class_id': 4153080,
-            'url': 'https://www.ravelry.com/discuss/hp-knitting-crochet-house-cup/4153080/151-17'
+            'url': 'https://www.ravelry.com/discuss/hp-knitting-crochet-house-cup/4153080/151-175'
         }
-        db = rw.analysePage(self.webpage, further_data)
+        cls.extracted = rw.analysePage(cls.webpage, cls.further_data)
 
         with open(Path('./test_data/extracted_db.json'), 'w') as jsonfile:
-            json.dump(db, jsonfile)
+            json.dump(cls.extracted, jsonfile)
 
-        for entry in db:
-            self.assertIn(entry, self.result)
+    def test_name(self):
+        names = []
+        for entry in self.extracted:
+            for truth in self.truths:
+                if entry['name'] == truth['name']:
+                    names.append(entry['name'])
+                    break
+        self.assertEqual(len(names), len(self.truths))
 
-        for entry in self.result:
-            self.assertIn(entry, db)
+        names = []
+        for truth in self.truths:
+            for entry in self.extracted:
+                if entry['name'] == truth['name']:
+                    names.append(entry['name'])
+                    break
+        self.assertEqual(len(names), len(self.extracted))
+
+    def test_post_data(self):
+        for entry in self.extracted:
+            self.assertEqual(entry['class_id'], self.further_data['class_id'])
+            for truth in self.truths:
+                if entry['name'] == truth['name']:
+                    self.assertEqual(entry['post_id'], truth['post_id'])
+                    self.assertEqual(entry['house'], truth['house'])
+                    self.assertEqual(entry['project'], truth['project'])
+                    self.assertEqual(entry['verb'], truth['verb'])
+                    self.assertEqual(entry['loved'], truth['loved'])
+                    break
+
+    def test_date(self):
+        for entry in self.extracted:
+            for truth in self.truths:
+                if entry['name'] == truth['name']:
+                    # I really don't know why I have to add 6h to the date. When I get the page with python, it has always an offset of 6h.
+                    # Propably it depends on the timezone ?!?!
+                    self.assertEqual(entry['date'], truth['date'] + 6*60*60)
+                    self.assertEqual(type(entry['date']), int)
+                    break
+
+    def test_url(self):
+        for entry in self.extracted:
+            for truth in self.truths:
+                if entry['name'] == truth['name']:
+                    self.assertEqual(entry['url'], truth['url'])
+                    self.assertIn(self.further_data['url'], truth['url'])
+                    break
 
 
 if __name__ == '__main__':
