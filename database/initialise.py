51796cda2dcd23ea834d7c4e73b92ce9e5f384f4
Robin <robin@braeutigam-nrw.de>
2022-08-27 22:01:32 +0200

    Initial trackers db model

--- database/initialise.py
+++ database/initialise.py
@@ -2,10 +2,12 @@ from pathlib import Path
 import sys
 sys.path.insert(0, str(Path.cwd()))
 from database import courses
+from database import trackers
 
 
 def initialise():
     courses.create_table()
+    trackers.create_table()
 
 
 def main():
