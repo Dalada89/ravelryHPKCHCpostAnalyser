93b967b308e2660d0e3286df95564564c1ddd3c5
Robin <robin@braeutigam-nrw.de>
2022-08-30 16:27:47 +0200

    Due to renaming the submissions

--- database/initialise.py
+++ database/initialise.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 import sys
 sys.path.insert(0, str(Path.cwd()))
-from database import courses, trackers, turn_ins
+from database import courses, trackers, submissions
 
 
 def initialise():
     courses.create_table()
     trackers.create_table()
-    turn_ins.create_table()
+    submissions.create_table()
 
 
 def main():
