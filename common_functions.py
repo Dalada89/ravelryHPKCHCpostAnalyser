67017bffe6233245852522acbc676474ccbf6781
Robin <robin@braeutigam-nrw.de>
2022-08-30 09:09:51 +0200

    a page can be given

--- common_functions.py
+++ common_functions.py
@@ -3,17 +3,25 @@ base_url = r'https://www.ravelry.com'
 group_url = r'/discuss/hp-knitting-crochet-house-cup'
 
 
-def create_url(ravelry_id, post_id=None, pageonly=False):
+def create_url(ravelry_id, post_id=None, pagegiven=None):
     url = '{base}{group}/{id}'
 
     url = url.format(base=base_url, group=group_url, id=ravelry_id)
-
+    page = None
     if post_id != None:
-        page = int(post_id/25)
-        s = page*25+1
-        e = page*25+25
+        page = int(post_id/25) + 1
+        if pagegiven != None:
+            if page != int(pagegiven):
+                msg = 'The post_id {pid} is not on page {given}.'
+                raise ValueError(msg.format(given=pagegiven, pid=post_id))
+    if pagegiven != None:
+        page = int(pagegiven)
+
+    if page != None:
+        s = (page - 1) * 25 + 1
+        e = (page - 1) * 25 + 25
         url += '/{s}-{e}'
-        if not pageonly:
+        if post_id != None:
             url += '#{p}'
         url = url.format(s=s, e=e, p=post_id)
 
@@ -21,7 +29,7 @@ def create_url(ravelry_id, post_id=None, pageonly=False):
 
 
 def test():
-    url = create_url(4256123, 1337, pageonly=False)
+    url = create_url(4256123)
     print(url)
 
 
